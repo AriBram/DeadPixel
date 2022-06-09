c0e1f2cc2e8a9c7028801fd2f1609fe06f251b93
Blind Bandit <9@spicy.team>
2022-06-09 16:29:32 +0300

    Health bar UI

--- Assets/Scripts/UI/Game/HealthBar.cs
+++ Assets/Scripts/UI/Game/HealthBar.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 
 public class HealthBar : MonoBehaviour {
     
     public List<GameObject> healthPoints;
     public List<GameObject> shields;
+
+    public TMP_Text hpCaption;
+    public Image icon;
+
+    public Sprite heartIcon;
+    public Sprite shieldIcon;
 
 
     void Start() {
@@ -21,11 +29,19 @@ public class HealthBar : MonoBehaviour {
         int playerHp = Player.Instance.health.hp;
         int shieldsCount = Player.Instance.health.shields.value;
 
+        Color UIcolor = GameData.Instance.qBits.Find(q => q.qType == Player.Instance.colorType).color;
+
+        icon.sprite = shieldsCount > 0 ? shieldIcon : heartIcon;
+
         for(int i = 0; i < playerHp; i++)
             healthPoints[i].SetActive(true);
 
-        for(int i = 0; i < shieldsCount; i++)
+        for(int i = 0; i < shieldsCount; i++) {
             shields[i].SetActive(true);
+            shields[i].GetComponent<Image>().color = UIcolor;
+        }
+
+        hpCaption.text = playerHp.ToString();
     }
 
 
