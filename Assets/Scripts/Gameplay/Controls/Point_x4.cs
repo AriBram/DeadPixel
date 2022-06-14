1783a6ef68ea12423a7149c32a536690b6edb88b
Blind Bandit <9@spicy.team>
2022-06-14 15:14:00 +0300

    Upper matrix for big x4 objects

---
+++ Assets/Scripts/Gameplay/Controls/Point_x4.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point_x4 : MonoBehaviour {
+
+    public int x;
+    public int y;
+    
+    public List<MovementPoint> points;
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Spider")) {
+            if(GameplayController.Instance.IsEnemyMove) {
+                foreach(var p in points) {
+                    if(p.isQbit) {
+                        QBit qBitToDestroy = Field.Instance.qBits.Find(q => q.x == p.x && q.y == p.y);
+                        qBitToDestroy.DestroyQbit();
+                    }
+                    /*Enemy e = other.gameObject.GetComponent<Enemy>();
+                    if(e.targetPoint.x == this.x && e.targetPoint.y == this.y)
+                        e.EndMove();*/
+                }
+            }
+        }
+    }
+}
